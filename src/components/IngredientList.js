3c1163d610936625bedc464b8cb7467285aca79a
Xavier Caldwell <xvrianreed@gmail.com>
2023-03-02 11:27:48 -0700

    updated anonymous function

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Ingredient from './Ingredient'
-export default function ({ingredients}) {
+export default function IngredientList({ingredients}) {
     const ingredientElements = ingredients.map(ingredient => {
         return <Ingredient key={ingredient.id} {...ingredient}/>
     })
