5c3838ca56a5f8509e2e3a3b428bb8563972dab7
Xavier Caldwell <xvrianreed@gmail.com>
2023-03-03 13:30:52 -0700

    add new button functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,93 @@
-import React from 'react'
-import RecipeList from './RecipeList';
-import '../css/app.css';
+import React, { useState } from "react";
+import RecipeList from "./RecipeList";
+import "../css/app.css";
+import { v4 as uuidv4 } from "uuid";
+
 function App() {
-  
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: "New",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "Instr.",
+      ingredients: [
+        {
+          id: uuidv4(),
+          name: "Name",
+          amount: "1Tbs",
+        },
+      ],
+    };
+    setRecipes([...recipes, newRecipe]);
+  }
+
   return (
-    <RecipeList recipes={sampleRecipes}/>
-    )
+    <RecipeList recipes={recipes} handleRecipeAdd={handleRecipeAdd} />
+  );
 }
 
 const sampleRecipes = [
   {
     id: 1,
-    name: 'Plain Chicken',
+    name: "Plain Chicken",
     servings: 3,
-    cookTime: '1:45',
+    cookTime: "1:45",
     instructions: "1. Put salt on chicken \n2. Put chicken in oven\n3. Eat",
-    ingredients:[
-{
-  id:1,
-  name: 'Chicken',
-  amount: '2 pounds'
-},
-{
-  id: 2,
-  name: 'Salt',
-  amount: '1 Tbs'
-}
-    ]
+    ingredients: [
+      {
+        id: 1,
+        name: "Chicken",
+        amount: "2 pounds",
+      },
+      {
+        id: 2,
+        name: "Salt",
+        amount: "1 Tbs",
+      },
+    ],
   },
   {
     id: 2,
-    name: 'Plain Pork',
+    name: "Plain Pork",
     servings: 5,
-    cookTime: '0:45',
+    cookTime: "0:45",
     instructions: "1. Put paprika on chicken \n2. Put pork in oven\n3. Eat",
-    ingredients:[
+    ingredients: [
       {
-        id:1,
-        name: 'Pork',
-        amount: '3 pounds'
+        id: 1,
+        name: "Pork",
+        amount: "3 pounds",
       },
       {
         id: 2,
-        name: 'Paprika',
-        amount: '2 Tbs'
-      }
-          ]
+        name: "Paprika",
+        amount: "2 Tbs",
+      },
+    ],
   },
   {
     id: 3,
-    name: 'Plain veal',
+    name: "Plain veal",
     servings: 3,
-    cookTime: '3:45',
-    instructions: "1. Put veal on tray \n2. Put veal in oven\n3. burn it. a lot.",
-    ingredients:[
+    cookTime: "3:45",
+    instructions:
+      "1. Put veal on tray \n2. Put veal in oven\n3. burn it. a lot.",
+    ingredients: [
       {
-        id:1,
-        name: 'Veal',
-        amount: '2 pounds'
+        id: 1,
+        name: "Veal",
+        amount: "2 pounds",
       },
       {
         id: 2,
-        name: 'Salt',
-        amount: '10 Tbs'
-      }
-          ]
+        name: "Salt",
+        amount: "10 Tbs",
+      },
+    ],
   },
-]
+];
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import IngredientList from './IngredientList'
+import React from "react";
+import IngredientList from "./IngredientList";
 export default function Recipe(props) {
-    const{
-        name, cookTime,servings, instructions, ingredients
-    } = props
+  const { name, cookTime, servings, instructions, ingredients } = props;
   return (
-    <div>
-       <div>
-        <h3>{name}</h3>
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
         <div>
-            <button>Edit</button>
-            <button>Delete</button>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button className="btn btn--danger">Delete</button>
         </div>
-        </div> 
-        <div>
-            <span>Cook Time:</span>
-            <span>{cookTime}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Cook Time:</span>
+        <span className="recipe__value">{cookTime}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Servings:</span>
+        <span className="recipe__value">{servings}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value recipe__instructions recipe__value--indented">{instructions}</div>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Ingredients:</span>
+        <div className="recipe__value recipe__value--indented ">
+          <IngredientList ingredients={ingredients} />
         </div>
-        <div>
-            <span>Servings:</span>
-            <span>{servings}</span>
-        </div>
-        <div>
-            <span>Instructions:</span>
-            <div>{instructions}</div>
-        </div>
-        <div>
-            <span>Ingredients:</span>
-            <div><IngredientList ingredients={ingredients}/></div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
